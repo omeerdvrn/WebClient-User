2c0afb4eee18609d8855a4c19f5352a9678cecd7
omerdavran <omeerdvrn@gmail.com>
2023-04-25 08:02:42 +0300

    async method impl, error handling impl

--- src/main/java/com/omeerdvrn/webclienttest/service/UserService.java
+++ src/main/java/com/omeerdvrn/webclienttest/service/UserService.java
@@ -2,15 +2,21 @@ package com.omeerdvrn.webclienttest.service;
 
 import com.omeerdvrn.webclienttest.model.User;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 
 @Service
 public class UserService {
 
     private final WebClient webClient;
+    private Logger logger = LoggerFactory.getLogger(UserService.class);
 
     public UserService(WebClient webClient) {
         this.webClient = webClient;
@@ -19,6 +25,14 @@ public class UserService {
     public Mono<User> GetRandomUserFromWebClient(){
          return webClient.get()
                  .retrieve()
-                 .bodyToMono(User.class);
+                 .bodyToMono(User.class)
+                 .retryWhen(Retry.fixedDelay(2, Duration.ofSeconds(1)))
+                 .doOnError(error -> logger.error("There is an error while sending request {}", error.getMessage()))
+                 .onErrorResume(error -> Mono.just(
+                         new User(null,
+                                 null,
+                                 null,
+                                 null,
+                                 null)));
     }
 }
